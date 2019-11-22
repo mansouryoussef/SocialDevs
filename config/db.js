ef179ace4b3c66547d1633c381df3a7dc0db8a46
Youssef <mansouryoussef7@outlook.com>
2019-11-22 13:30:24 +0200

    Add: MongoDB atlas database connection

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// DB connection
+
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, { useUnifiedTopology: true });
+
+		console.log('Connected to Mongo Atlas');
+	} catch (e) {
+		console.error(e.message);
+
+		// Exit process with failures
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
