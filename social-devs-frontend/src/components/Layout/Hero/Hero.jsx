3f27ef069e7ed9fea8d7474515e53c6bd91463bd
Youssef Mansour <contact@youssef.fi>
2020-01-17 14:34:32 +0200

    Refactored: Hero component to use sass modules

--- social-devs-frontend/src/components/Layout/Hero/Hero.jsx
+++ social-devs-frontend/src/components/Layout/Hero/Hero.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import './HeroStyles.scss';
+import Styles from './Hero.module.scss';
+
 import postSvg from 'assets/img/icons/hero-post.svg';
 import heroSvg from 'assets/img/icons/hero.svg';
 
 export default function Hero() {
 	return (
-		<div className='hero-container'>
-			<img src={postSvg} alt='post svg' className='hero-container__post' />
-			<img src={heroSvg} alt='hero svg' className='hero-container__hero' />
+		<div className={Styles.heroContainer}>
+			<img src={postSvg} alt='post svg' className={Styles.singlePostImg} />
+			<img src={heroSvg} alt='hero svg' className={Styles.heroImg} />
 		</div>
 	);
 }
