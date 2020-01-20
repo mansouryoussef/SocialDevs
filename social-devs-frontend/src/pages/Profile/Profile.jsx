04b198722613d9675cfc66a80cddd08836340291
Youssef Mansour <contact@youssef.fi>
2020-01-20 18:13:39 +0200

    Refactored: Profile page to use sass modules

--- social-devs-frontend/src/pages/Profile/Profile.jsx
+++ social-devs-frontend/src/pages/Profile/Profile.jsx
@@ -1,31 +1,30 @@
 import React, { useContext, useState, useEffect } from 'react';
-import './ProfileStyles.scss';
+import Styles from './Profile.module.scss';
 
 import deleteprofile from 'assets/img/icons/deleteprofile.svg';
-import { DataContext } from 'contexts/DataContext';
 import CreateProfileForm from 'components/Profile/CreateProfileForm/CreateProfileForm';
-import { useHistory } from 'react-router-dom';
 import ProfileTable from 'components/Profile/ProfileTable/ProfileTable';
-import { handleDeleteAccount } from 'service/profile';
 import ProfileInfoList from 'components/Profile/ProfileInfoList/ProfileInfoList';
 import IconButtonDanger from 'components/Shared/Buttons/IconButtonDanger/IconButtonDanger';
 import Disclaimer from 'components/Shared/Disclaimer/Disclaimer';
+import { DataContext } from 'contexts/DataContext';
+import { useHistory } from 'react-router-dom';
+import { handleDeleteAccount } from 'service/profile';
 
 export default function Profile() {
+	const history = useHistory();
+
 	const { userProfile, setIsLoggedin, user } = useContext(DataContext);
 	const [showEditForm, setShowEditForm] = useState(
 		userProfile.title === undefined
 	);
-
-	const [showError, setShowError] = useState(false);
+	const [isGuest, setIsGuest] = useState(false);
 
 	useEffect(() => {
 		userProfile.title === undefined
 			? setShowEditForm(true)
 			: setShowEditForm(false);
 	}, [userProfile]);
-
-	const history = useHistory();
 
 	const expTableProps = {
 		type: 'experience',
@@ -40,12 +39,12 @@ export default function Profile() {
 	};
 
 	return (
-		<div className='profile-page'>
-			<h1 className='profile-page__title'>Profile</h1>
+		<div className={Styles.profilePage}>
+			<h1 className={Styles.title}>Profile</h1>
 
-			<div className='profile-page__content'>
-				<h2 className='profile-page__content__form-title'>
-					{!showEditForm ? 'Profile information:' : 'Create a profile:'}
+			<div className={Styles.content}>
+				<h2 className={Styles.formTitle}>
+					{showEditForm ? 'Create a profile:' : 'Profile information:'}
 				</h2>
 
 				{showEditForm ? (
@@ -63,14 +62,15 @@ export default function Profile() {
 				<IconButtonDanger
 					onClick={() => {
 						user.name === 'Guest'
-							? setShowError(true)
+							? setIsGuest(true)
 							: handleDeleteAccount(setIsLoggedin, history);
 					}}
 					icon={deleteprofile}
 					text='Delete my account!'
 					filled
 				/>
-				{showError && (
+
+				{isGuest && (
 					<Disclaimer err='Sorry, guests are not allowed to delete their accounts for others to use it.' />
 				)}
 			</div>
